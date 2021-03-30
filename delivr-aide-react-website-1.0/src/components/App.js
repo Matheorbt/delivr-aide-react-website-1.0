6db13d42ef617d181bbbe77da1ea276123f010af
MATHEO ROBERT <matho.robert@gmail.com>
2021-03-30 19:49:48 +0200

    Done most of the style part (~50%)

--- delivr-aide-react-website-1.0/src/components/App.js
+++ delivr-aide-react-website-1.0/src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Glider, { GliderMethods } from 'react-glider';
 import logo from '../props/logo/logo.png';
-import heroCover from '../props/img/Delivry-bag.jpeg'
+import heroCover from '../props/img/delivery-bag.jpeg'
+import freshProduct from '../props/img/fresh-product.png'
+import hygieneProduct from '../props/img/produit_hygiene.jpg'
+import franceMap from '../props/img/map-france-pin.png'
 
 function App() {
+    var deliveryMan = 14;
+    var cityAvailaible = 6;
+    var totalDelivery = 200;
     return (
         <>
             <nav className="navbar-wrapper">
@@ -15,16 +22,39 @@ function App() {
                         <div className="left-column">
                             <p className="hero-title">Besoin d'aide ?<br />Delivr'aide</p>
                             <p className="hero-description">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum distinctio officiis aliquam placeat? Explicabo temporibus aliquid aliquam delectus facilis quod ab perferendis fugit modi, consectetur itaque, repudiandae eligendi quam amet?
+                                Reçevez gratuitement votre panier repas et autres produits de première nécessité directement à votre domicile. Pour bénéficier d'un kit solidaire, renseignez simplement les informations en cliquant sur le bouton ci-dessous et nous vous contacterons sous 24h pour vous donner la date et l'heure de la livraison. <br />À très vite !
                     </p>
                         </div>
                         <div className="right-column">
                             <img src={heroCover} alt="delivr'aide logo" className="hero-banner" />
                         </div>
                     </section>
+                    <button className="btn-primary-blue introduction-btn">Recevoir mon kit !</button>
                 </div>
-                <button className="btn-primary-blue introduction-btn">Recevoir mon kit !</button>
             </div>
+            <div className="info-banner banner-fresh-product">
+                <div className="info-banner-column-wrapper">
+                    <p className="banner-text">Nous vous proposons des produits frais.</p>
+                    <img className="banner-img" alt="fresh product" src={freshProduct}></img>
+                </div>
+            </div>
+            <div className="available-city">
+                <img src={franceMap} alt="map of france with every handled city" className="france-map-img"></img>
+                <p className="france-map-text">Là où il y’a besoin d’aide Delivr’aide est là.</p>
+            </div>
+            <div className="info-banner-reverse banner-fresh-product">
+                <div className="info-banner-column-wrapper">
+                    <p className="banner-text">Nous vous offrons aussi des produits de première nécessité.</p>
+                    <img className="banner-img" alt="hygiene product" src={hygieneProduct}></img>
+                </div>
+            </div>
+            <section className="delivraide-info">
+                <p className="delivraide-info-title"><span className="logo-delivr">Delivr</span><span className="logo-apostrophe">'</span><span className="logo-aide">aide</span><span className="delivraide-info-title-stroke"> c'est quoi ?</span></p>
+                <p className="delivraide-info-stat-delivery-man"><span className="delivraide-info-stat-stroke">{deliveryMan}</span> Livreurs bénévoles</p>
+                <p className="delivraide-info-stat-availaible-city"><span className="delivraide-info-stat-stroke">{cityAvailaible}</span> Villes</p>
+                <p className="delivraide-info-stat-total-delivery"><span className="delivraide-info-stat-stroke">{totalDelivery}</span> Livraisons</p>
+                <p className="delivraide-info-title"><span className="logo-delivr">Delivr</span><span className="logo-apostrophe">'</span><span className="logo-aide">aide</span><span className="delivraide-info-title-stroke"> c'est surtout</span></p>
+            </section>
         </>
     )
 }
