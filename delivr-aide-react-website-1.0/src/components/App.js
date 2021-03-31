507383df1d575707b39df015e16f01329fab2a0d
MATHEO ROBERT <matho.robert@gmail.com>
2021-03-31 19:04:49 +0200

    added autoplay on carousel

--- delivr-aide-react-website-1.0/src/components/App.js
+++ delivr-aide-react-website-1.0/src/components/App.js
@@ -31,7 +31,9 @@ function App() {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
     };
     return (
         <>
